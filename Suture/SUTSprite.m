145d6cc4f4bf7573625b07d7eccd42c4088cc40e
James Campbell <jscampbell.05@googlemail.com>
2015-03-12 14:26:12 +0000

    Implement coding protocol.

--- Suture/SUTSprite.m
+++ Suture/SUTSprite.m
@@ -10,4 +10,23 @@
 
 @implementation SUTSprite
 
+#pragma mark NSCoding
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    
+    if (self)
+    {
+        self.fileName = [aDecoder decodeObjectForKey:@"fileName"];
+    }
+    
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [aCoder encodeObject:self.fileName forKey:@"fileName"];
+}
+
 @end

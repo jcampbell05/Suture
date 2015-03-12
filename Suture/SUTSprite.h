8982010b160c21e758590ea056e192be092b3d63
James Campbell <jscampbell.05@googlemail.com>
2015-03-12 14:22:35 +0000

    Adds sprite object.

---
+++ Suture/SUTSprite.h
@@ -0,0 +1,13 @@
+//
+//  SUTSprite.h
+//  Suture
+//
+//  Created by James Campbell on 12/03/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SUTSprite : NSObject
+
+@end
---
+++ Suture/SUTSprite.m
@@ -0,0 +1,13 @@
+//
+//  SUTSprite.m
+//  Suture
+//
+//  Created by James Campbell on 12/03/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import "SUTSprite.h"
+
+@implementation SUTSprite
+
+@end

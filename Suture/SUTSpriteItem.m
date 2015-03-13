a7ed30619e55b0c837427dfd34e2c3afc6e2dbbe
James Campbell <jscampbell.05@googlemail.com>
2015-03-13 11:15:25 +0000

    Adds extra classes for collection view.

---
+++ Suture/SUTSpriteItem.m
@@ -0,0 +1,22 @@
+//
+//  SUTSpriteItem.m
+//  Suture
+//
+//  Created by James Campbell on 13/03/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import "SUTSpriteItem.h"
+
+@interface SUTSpriteItem ()
+
+@end
+
+@implementation SUTSpriteItem
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
---
+++ Suture/SUTSpriteView.m
@@ -0,0 +1,19 @@
+//
+//  SUTSpriteView.m
+//  Suture
+//
+//  Created by James Campbell on 13/03/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import "SUTSpriteView.h"
+
+@implementation SUTSpriteView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end

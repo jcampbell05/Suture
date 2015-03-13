3dc4d4db973dfde103b9683b93906f021513a1fd
James Campbell <jscampbell.05@googlemail.com>
2015-03-13 10:49:27 +0000

    Moves view into view controller.

--- Suture/SUTWindowController.m
+++ Suture/SUTWindowController.m
@@ -7,10 +7,12 @@
 //
 
 #import "SUTWindowController.h"
-#import "SUTWindow.h"
+
+#import "SUTEditorView.h"
 
 @interface SUTWindowController ()
 
+@property (nonatomic, strong) SUTEditorView *editorView;
 
 @end
 
@@ -25,25 +27,33 @@
     {
         CGSize windowSize = CGSizeMake(550.0f,
                                        450.0f);
-        self.window = [[SUTWindow alloc] initWithContentRect:(NSRect){NSZeroPoint, windowSize}
-                                                   styleMask:(NSTitledWindowMask |
-                                                              NSClosableWindowMask |
-                                                              NSMiniaturizableWindowMask |
-                                                              NSResizableWindowMask)
-                                                     backing:NSBackingStoreBuffered
-                                                       defer:NO];
+        self.window = [[NSWindow alloc] initWithContentRect:(NSRect){NSZeroPoint, windowSize}
+                                                  styleMask:(NSTitledWindowMask |
+                                                             NSClosableWindowMask |
+                                                             NSMiniaturizableWindowMask |
+                                                             NSResizableWindowMask)
+                                                    backing:NSBackingStoreBuffered
+                                                      defer:NO];
         self.window.contentMinSize = windowSize;
         [self.window center];
+        
+        [self.window.contentView addSubview:self.editorView];
     }
     
     return self;
 }
 
+#pragma mark - EditorView
 
-- (void)windowDidLoad
+- (SUTEditorView *)editorView
 {
-    [super windowDidLoad];
+    if (!_editorView)
+    {
+        _editorView = [[SUTEditorView alloc] initWithFrame:((NSView*)self.window.contentView).bounds];
+        _editorView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+    }
     
+    return _editorView;
 }
 
 @end

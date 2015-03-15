38de727acc87b1d07236992f7dbcc7f3e0cc6f90
James Campbell <jscampbell.05@googlemail.com>
2015-03-15 13:27:46 +0000

    Building flow layout.

--- Suture/SUTCollectionViewSpriteLayout.m
+++ Suture/SUTCollectionViewSpriteLayout.m
@@ -8,6 +8,36 @@
 
 #import "SUTCollectionViewSpriteLayout.h"
 
+@interface SUTCollectionViewSpriteLayout ()
+
+@property (nonatomic, strong)  NSMutableArray *attributes;
+
+@end
+
 @implementation SUTCollectionViewSpriteLayout
+
+#pragma mark - Layout
+
+- (void)prepareLayout
+{
+    NSInteger numberOfItems = [self.collectionView numberOfItemsInSection:0];
+    
+    for (NSInteger index = 0; index < numberOfItems; index++)
+    {
+        
+    }
+}
+
+#pragma mark - Attributes
+
+- (NSMutableArray *)attributes
+{
+    if (!_attributes)
+    {
+        _attributes = [[NSMutableArray alloc] init];
+    }
+    
+    return _attributes;
+}
 
 @end

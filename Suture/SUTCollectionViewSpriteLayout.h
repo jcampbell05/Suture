370b739dd4c640daa444d2fbc9cd6f08ed4ab202
James Campbell <jscampbell.05@googlemail.com>
2015-03-16 22:15:43 +0000

    Layout object.

--- Suture/SUTCollectionViewSpriteLayout.h
+++ Suture/SUTCollectionViewSpriteLayout.h
@@ -6,20 +6,9 @@
 //  Copyright (c) 2015 Unii. All rights reserved.
 
 #import "JNWCollectionViewLayout.h"
-
 #import "SUTSpriteLayout.h"
 
-@protocol SUTCollectionViewSpriteLayoutDelegate <NSObject>
-
-- (CGSize)collectionView:(JNWCollectionView *)collectionView
-  sizeForItemAtIndexPath:(NSIndexPath *)indexPath;
-
-@end
-
 @interface SUTCollectionViewSpriteLayout : JNWCollectionViewLayout
-
-@property (nonatomic, weak) id<SUTCollectionViewSpriteLayoutDelegate> delegate;
-@property (nonatomic, assign) SUTSpriteLayoutOrientation orientation;
 
 @property (nonatomic, strong) SUTSpriteLayout *layout;
 

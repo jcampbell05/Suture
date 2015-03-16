5f9f5816efce5ca205768bf741439acfc3edce93
James Campbell <jscampbell.05@googlemail.com>
2015-03-16 23:16:38 +0000

    Exporter added.

---
+++ Suture/SUTExporter.h
@@ -0,0 +1,13 @@
+//
+//  SUTExporter.h
+//  Suture
+//
+//  Created by James Campbell on 16/03/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SUTExporter <NSObject>
+
+@end

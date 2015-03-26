db0feae9d659a013ed868514d56749dd39bad44b
James Campbell <jscampbell.05@googlemail.com>
2015-03-26 01:13:12 +0000

    Adds in progress panel.

---
+++ Suture/SUTProgressPanel.h
@@ -0,0 +1,13 @@
+//
+//  SUTProgressPanel.h
+//  Suture
+//
+//  Created by James Campbell on 26/03/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SUTProgressPanel : NSPanel
+
+@end
---
+++ Suture/SUTProgressPanel.m
@@ -0,0 +1,13 @@
+//
+//  SUTProgressPanel.m
+//  Suture
+//
+//  Created by James Campbell on 26/03/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import "SUTProgressPanel.h"
+
+@implementation SUTProgressPanel
+
+@end

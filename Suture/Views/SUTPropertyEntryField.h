fee738f53fd679868c0f89c94e89dae60cb8352a
James Campbell <jscampbell.05@googlemail.com>
2015-05-22 15:10:22 +0100

    Creating property entry field.

---
+++ Suture/Views/SUTPropertyEntryField.h
@@ -0,0 +1,13 @@
+//
+//  SUTPropertyEntryField.h
+//  Suture
+//
+//  Created by James Campbell on 22/05/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SUTPropertyEntryField : NSView
+
+@end
---
+++ Suture/Views/SUTPropertyEntryField.m
@@ -0,0 +1,19 @@
+//
+//  SUTPropertyEntryField.m
+//  Suture
+//
+//  Created by James Campbell on 22/05/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import "SUTPropertyEntryField.h"
+
+@implementation SUTPropertyEntryField
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end

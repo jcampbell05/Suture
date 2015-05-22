b8a2f7a64140186e32731a0e40a96b2927e729ee
James Campbell <jscampbell.05@googlemail.com>
2015-05-22 11:34:30 +0100

    Adds in basic title.

--- Suture/Views/SUTPropertyView.m
+++ Suture/Views/SUTPropertyView.m
@@ -8,6 +8,12 @@
 
 #import "SUTPropertyView.h"
 
+@interface SUTPropertyView ()
+
+@property (nonatomic, strong) NSTextField *specificationsTitleView;
+
+@end
+
 @implementation SUTPropertyView
 
 #pragma mark - Init
@@ -23,9 +29,39 @@
                                                      green:58.0f/255.0f
                                                       blue:71.0f/255.0f
                                                      alpha:1.0f].CGColor;
+        
+        [self addSubview:self.specificationsTitleView];
     }
     
     return self;
+}
+
+#pragma mark - Specifications
+
+- (NSTextField *)specificationsTitleView
+{
+    if (!_specificationsTitleView)
+    {
+        _specificationsTitleView = [[NSTextField alloc] init];
+        
+        _specificationsTitleView.alignment = NSCenterTextAlignment;
+        _specificationsTitleView.bezeled = NO;
+        _specificationsTitleView.textColor = [NSColor whiteColor];
+        _specificationsTitleView.drawsBackground = NO;
+        _specificationsTitleView.editable = NO;
+        _specificationsTitleView.selectable = NO;
+        
+        _specificationsTitleView.stringValue = NSLocalizedString(@"specifications_nav",
+                                                         nil);
+        
+        [_specificationsTitleView sizeToFit];
+        
+        CGRect frame = _specificationsTitleView.frame;
+        frame.size.width = self.bounds.size.width;
+        _specificationsTitleView.frame = frame;
+    }
+    
+    return _specificationsTitleView;
 }
 
 @end

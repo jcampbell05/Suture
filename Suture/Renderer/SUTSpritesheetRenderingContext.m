939dc254c0cc40ab0872ef0349937da6aea1a9e5
James Campbell <jscampbell.05@googlemail.com>
2015-06-11 17:17:09 +0100

    Preparing context object.

---
+++ Suture/Renderer/SUTSpritesheetRenderingContext.m
@@ -0,0 +1,13 @@
+//
+//  SUTSpritesheetRenderingContext.m
+//  Suture
+//
+//  Created by James Campbell on 11/06/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import "SUTSpritesheetRenderingContext.h"
+
+@implementation SUTSpritesheetRenderingContext
+
+@end

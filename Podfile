800b0a156503c334b39a63d3ee42fd3d710de8de
James Campbell <jscampbell.05@googlemail.com>
2015-06-11 13:46:27 +0100

    Preparing for switching from JNWCollectionView.

--- Podfile
+++ Podfile
@@ -1,6 +1,5 @@
 source 'https://github.com/CocoaPods/Specs.git'
 
-pod 'JNWCollectionView'
 pod 'PureLayout'
 pod 'Sparkle', '~> 1.9'
 pod 'ReactiveCocoa'
---
+++ Suture/Views/SUTSpritesheetView.h
@@ -0,0 +1,13 @@
+//
+//  SUTSpritesheetView.h
+//  Suture
+//
+//  Created by James Campbell on 11/06/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SUTSpritesheetView : NSView
+
+@end

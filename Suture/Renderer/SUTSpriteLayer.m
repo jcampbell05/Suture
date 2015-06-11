7c0094c86ca4cec059161dbb7d3fba9e03f8438b
James Campbell <jscampbell.05@googlemail.com>
2015-06-11 17:26:38 +0100

    Preparing for sprite rendering.

---
+++ Suture/Renderer/SUTSpriteLayer.m
@@ -0,0 +1,13 @@
+//
+//  SUTSpriteLayer.m
+//  Suture
+//
+//  Created by James Campbell on 11/06/2015.
+//  Copyright (c) 2015 James Campbell. All rights reserved.
+//
+
+#import "SUTSpriteLayer.h"
+
+@implementation SUTSpriteLayer
+
+@end

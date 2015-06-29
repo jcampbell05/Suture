b4b1eed7bbdb888385e473cb83d2b26a2070213b
James Campbell <jscampbell.05@googlemail.com>
2015-06-29 13:53:40 +0100

    Adds some tweaks to the layout.

--- Suture/Layouts/SUTSpritesheetLayout.m
+++ Suture/Layouts/SUTSpritesheetLayout.m
@@ -52,64 +52,58 @@
 
 - (void)prepareLayout
 {
-//    self.contentSize = CGSizeZero;
-//    self.cellSize = CGSizeZero;
-//    [self.layoutAttributes removeAllObjects];
-//    
-//    CGPoint spriteOffset = CGPointZero;
-//    NSInteger numberOfSprites = [self.delegate numberOfSprites];
-//    
-//    for (NSInteger spriteIndex = 0; spriteIndex < numberOfSprites; spriteIndex ++)
-//    {
-//        CGSize spriteSize = [self.delegate sizeForSpriteAtIndex:spriteIndex];
-//        self.cellSize = (CGSize)
-//        {
-//            MAX(self.cellSize.width, spriteSize.width),
-//            MAX(self.cellSize.height, spriteSize.height)
-//        };
-//    }
-//    
-//    for (NSInteger spriteIndex = 0; spriteIndex < numberOfSprites; spriteIndex ++)
-//    {
-//        CGSize spriteSize = [self.delegate sizeForSpriteAtIndex:spriteIndex];
-//        
-//        CGPoint cellPosition = CGPointZero;
-//        cellPosition.x = self.cellSize.width * (spriteIndex * self.transformMultiplier.x);
-//        cellPosition.y = self.cellSize.height * (spriteIndex * self.transformMultiplier.y);
-//        
-//        CGPoint spritePosition = cellPosition;
-//        spritePosition.x += (self.cellSize.width / 2) - (spriteSize.width / 2);
-//        spritePosition.y += (self.cellSize.height / 2) - (spriteSize.height / 2);
-//        
-//        SUTSpriteLayoutAttribute *attribute = [[SUTSpriteLayoutAttribute alloc] init];
-//        attribute.spriteFrame = (CGRect){spritePosition, spriteSize};
-//        
-//        [self.layoutAttributes addObject:attribute];
-//        
-//        spriteOffset.x += self.cellSize.width * self.transformMultiplier.x;
-//        spriteOffset.y += self.cellSize.height * self.transformMultiplier.y;
-//    }
-//    
-//    CGPoint contentSizeMultiplier = (CGPoint)
-//    {
-//        MAX(1, numberOfSprites * self.transformMultiplier.x),
-//        MAX(1, numberOfSprites * self.transformMultiplier.y)
-//    };
-//    
-//    self.contentSize = CGSizeMake(self.cellSize.width * contentSizeMultiplier.x,
-//                                  self.cellSize.height * contentSizeMultiplier.y);
+    self.contentSize = CGSizeZero;
+    self.cellSize = CGSizeZero;
+    [self.layoutAttributes removeAllObjects];
+    
+    CGPoint spriteOffset = CGPointZero;
+    NSInteger numberOfSprites = [self.delegate numberOfSprites];
+    
+    for (NSInteger spriteIndex = 0; spriteIndex < numberOfSprites; spriteIndex ++)
+    {
+        CGSize spriteSize = [self.delegate sizeForSpriteAtIndex:spriteIndex];
+        self.cellSize = (CGSize)
+        {
+            MAX(self.cellSize.width, spriteSize.width),
+            MAX(self.cellSize.height, spriteSize.height)
+        };
+    }
+    
+    for (NSInteger spriteIndex = 0; spriteIndex < numberOfSprites; spriteIndex ++)
+    {
+        CGSize spriteSize = [self.delegate sizeForSpriteAtIndex:spriteIndex];
+        
+        CGPoint cellPosition = CGPointZero;
+        cellPosition.x = self.cellSize.width * (spriteIndex * self.transformMultiplier.x);
+        cellPosition.y = self.cellSize.height * (spriteIndex * self.transformMultiplier.y);
+        
+        CGPoint spritePosition = cellPosition;
+        spritePosition.x += (self.cellSize.width / 2) - (spriteSize.width / 2);
+        spritePosition.y += (self.cellSize.height / 2) - (spriteSize.height / 2);
+        
+        SUTSpriteLayoutAttribute *attribute = [[SUTSpriteLayoutAttribute alloc] init];
+        attribute.spriteFrame = (CGRect){spritePosition, spriteSize};
+        
+        [self.layoutAttributes addObject:attribute];
+        
+        spriteOffset.x += self.cellSize.width * self.transformMultiplier.x;
+        spriteOffset.y += self.cellSize.height * self.transformMultiplier.y;
+    }
+    
+    CGPoint contentSizeMultiplier = (CGPoint)
+    {
+        MAX(1, numberOfSprites * self.transformMultiplier.x),
+        MAX(1, numberOfSprites * self.transformMultiplier.y)
+    };
+    
+    self.contentSize = CGSizeMake(self.cellSize.width * contentSizeMultiplier.x,
+                                  self.cellSize.height * contentSizeMultiplier.y);
 }
 
 - (CGRect)frameForSpriteAtIndex:(NSInteger)index
 {
-//    SUTSpriteLayoutAttribute *attribute = self.layoutAttributes[index];
-//    
-//    attribute.spriteFrame = SUTFlipCGRect(attribute.spriteFrame,
-//                                          self.contentSize);
-//    
-//    return attribute.spriteFrame;
-    
-    return CGRectZero;
+    SUTSpriteLayoutAttribute *attribute = self.layoutAttributes[index];
+    return attribute.spriteFrame;
 }
 
 @end

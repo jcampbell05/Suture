9b018d357998a3dad3f74807fb2ec339cd1a3a30
James Campbell <james@supmenow.com>
2015-12-08 01:08:38 +0000

    Updates pod version.

--- Podfile
+++ Podfile
@@ -2,4 +2,3 @@ source 'https://github.com/CocoaPods/Specs.git'
 
 pod 'PureLayout'
 pod 'Sparkle', '~> 1.9'
-pod 'ReactiveCocoa'
--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -12,30 +12,6 @@
 #define COCOAPODS_VERSION_MINOR_PureLayout 0
 #define COCOAPODS_VERSION_PATCH_PureLayout 6
 
-// ReactiveCocoa
-#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
-#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa 2
-#define COCOAPODS_VERSION_MINOR_ReactiveCocoa 5
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 0
-
-// ReactiveCocoa/Core
-#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_Core
-#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_Core 2
-#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_Core 5
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 0
-
-// ReactiveCocoa/UI
-#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_UI
-#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_UI 2
-#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_UI 5
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_UI 0
-
-// ReactiveCocoa/no-arc
-#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_no_arc
-#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_no_arc 2
-#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 5
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 0
-
 // Sparkle
 #define COCOAPODS_POD_AVAILABLE_Sparkle
 #define COCOAPODS_VERSION_MAJOR_Sparkle 1
